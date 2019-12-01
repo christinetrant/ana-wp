1c2b30218cf8cce8490d7fc6f3cedde3f8bfff32
Christine Trant <christinetrant@gmail.com>
2019-12-01 20:32:52 +0000

    added content.php page and changed about.php so that the page can be edited by client

--- about.php
+++ about.php
@@ -7,50 +7,20 @@
 <section class="site-section">
   <div class="container">
     <div class="row align-items-center">
+      
       <div class="col-md-12 col-lg-5 mb-5 mb-lg-0">
         <h2 class="mb-3 text-primary">About Ana</h2>
-        <p class="lead">Mission Statement</p>
-        <p class="mb-4">Femaleboxingcoach wants to run and create as many women only sessions around south London to enjoy the sport, skill and benefits of boxing. Offering a safe space and sessions which empower individual woman, mentally and physically within a sport that men dominate.</p>
+        
+        <!--<p class="lead">Mission Statement</p>-->
+        <?php
 
-        <p class="mb-4">My sessions are for all abilities and fitness levels, a full body workout, believe. Women can come and learn defences, skill, combinations, foot work, partner work and pad work. There is no pressure and is non- contact it's very much about focusing on fitness, helping you build confidence, making new friends and strengthening your body and mind.</p>
+      /* Start the Loop */
+      while ( have_posts() ) : the_post();
 
-        <p class="mb-4"><strong>My ethos is: </strong>To work from the inside out, <q> if you feel good mentally, then you look good</q></p>
+        get_template_part( 'content', 'page' );
 
-        <p class="mb-4">I wanted to be a female only boxing coach because I can motivate individuals and especially reassuring woman that they can be great at boxing, a sport that is dominated by men.</p>
-
-        <p class="mb-4">I want to and my plan is to reach as many women as I can so they can also feel the benefits of boxing and continue to grind on in this busy society we live in.</p>
-
-      </div>
-      <div class="col-md-12 col-lg-6 ml-auto">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery/boxing-2.jpeg" alt="Image" class="img-fluid">
-      </div>
-
-      <div class="col-md-12 col-lg-12 mb-5 mb-lg-0">
-        <p class="mb-4">When it comes to boxing and women the most common statements I hear are:</p>
-        <ul class="site-block-check">
-          <li><q>I am not fit enough</q> <br>I say, Strength is not always visual it starts in the mind.</li>
-          <li><q>My coordination isn’t good</q> <br>I say, if you have the willingness to learn you will do just that, if you fail you get up and try again.</li>
-        </ul>
-
-        <p class="mb-4">I bring my thirst for life and perseverance into coaching, helping clients/participants to not give up and keep trying their best.</p>
-
-        <p class="mb-4">What is your excuse? No matter your size, race or fitness ability, give it a try, <strong>what have you got to lose?</strong></p>
-
-        <p class="mb-4">I am a passionate and dedicated level 2 England boxing coach, level 3 personal trainer. A coach who smashes barriers, Inclusion is key Deaf clients welcome qualified level 2 British Sign Language (BSL) and LGBT women welcome.</p>
-
-        <p class="mb-4">Come feel the benefits and get that endorphin rush for yourself!</p>
-
-        <p class="lead">My qualifications:</p>
-        <ul class="site-block-check">
-          <li>Level 2 Fitness instructor</li>
-          <li>Level 3 Personal trainer</li>
-          <li>Level 2 England boxing coach</li>
-          <li>Safeguarding</li>
-          <li>First aid</li>
-          <li>BA community and youth work</li>
-          <li>Level 2 British sign language</li>
-        </ul>
-
+      endwhile; // End of the loop.
+    ?>
         <p><a href="http://www.femaleboxingcoach.com/classes/" class="btn btn-primary pill px-4">Classes</a></p>
       </div>
     </div>
---
+++ content.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ */
+
+?>
+
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Post title. Only visible to screen readers. */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'ana-wp' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
+
+// 		wp_link_pages(
+// 			array(
+// 				'before' => '<div class="page-links">' . __( 'Pages:', 'ana-wp' ),
+// 				'after'  => '</div>',
+// 			)
+// 		);
+		?>
+		
